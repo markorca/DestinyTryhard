92386acfd1eaf47a431de653a3a94b0d399e4bde
Markorca <729613286@qq.com>
2019-08-22 12:33:57 +0800

    add mainfest function

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,13 +7,25 @@ use App\Repository\BungieRepository;
 
 class TestController extends Controller
 {
-    public function __construct() 
+    public function __construct(BungieRepository $bungieRepository) 
     {
-        $this->bungieRepo = new BungieRepository;
+        $this->bungieRepo = $bungieRepository;
     }
 
     public function test()
     {
-        $this->bungieRepo->getDestiny2Manifest();
+        $response = $this->bungieRepo->searchDestinyPlayer(2, 'Markorca');
+        $membershipId = $response['Response']['0']['membershipId'];
+
+        $response = $this->bungieRepo->getDestiny2Profile(2, $membershipId);
+        $characterIds = $response['Response']['profile']['data']['characterIds'];
+
+        // $response = $this->bungieRepo->getHistoricalStatsDefinition();
+        $response = $this->bungieRepo->getHistoricalStatsForAccount(2, $membershipId, 'General');
+        echo json_encode($response);exit;
+        // $this->bungieRepo->getActivityHistory(2, $membershipId, $characterIds[0], 5);
+
+        $response = $this->bungieRepo->getDefinition('DestinyInventoryItemDefinition');
+        echo json_encode($response);exit;
     }
 }
--- app/Repository/BungieRepository.php
+++ app/Repository/BungieRepository.php
@@ -2,6 +2,10 @@
 
 namespace App\Repository;
 
+use Cache;
+use ZipArchive;
+use SQLite3;
+
 class BungieRepository 
 {
 	function __construct() 
@@ -41,22 +45,103 @@ class BungieRepository
 		}
 	}
 
-	public function searchUsers($searchString) 
+	// public function searchUsers($searchString) 
+	// {
+	// 	$path = "/User/SearchUsers/?q=" . urlencode($searchString);
+
+	// 	$response = $this->execCurl($path);
+
+	// 	echo json_encode($response);exit;
+
+	// }
+
+	public function checkManifest() 
 	{
-		$path = "/User/SearchUsers/?q=" . urlencode($searchString);
+		$result = $this->getManifest("/Destiny2/Manifest/");
 
-		$response = $this->execCurl($path);
+		$database = $result['Response']['mobileWorldContentPaths']['zh-chs'];
 
-		echo json_encode($response);exit;
+		if ($database != Cache::get('database')) {
+			$tables = $this->updateManifest($database);
 
+			Cache::put('database', $database);
+			Cache::put('tables', $tables);
+		}
 	}
 
-	public function getDestiny2Manifest()
+	private function getManifest($path)
 	{
-		$path = "/Destiny2/Manifest/";
-
 		$response = $this->execCurl($path);
-		echo json_encode($response);exit;
+		return $response;
+	}
+
+	private function updateManifest($url) 
+	{
+		// $ch = curl_init('https://www.bungie.net' . $url);
+		// curl_setopt_array($ch, array(
+		// 	CURLOPT_RETURNTRANSFER => true
+		// ));
+		// $data = curl_exec($ch);
+		// curl_close($ch);
+
+		$data = file_get_contents('https://www.bungie.net' . $url);
+
+		$cacheFilePath = '../storage/'.pathinfo($url, PATHINFO_BASENAME);
+		if (!file_exists(dirname($cacheFilePath))) mkdir(dirname($cacheFilePath), 0777, true);
+		file_put_contents($cacheFilePath.'.zip', $data);
+	
+		$zip = new ZipArchive();
+		if ($zip->open($cacheFilePath.'.zip') === TRUE) {
+			$zip->extractTo('../storage/');
+			$zip->close();
+		}
+	
+		$tables = array();
+		if ($db = new SQLite3($cacheFilePath)) {
+			$result = $db->query("SELECT name FROM sqlite_master WHERE type='table'");
+			while($row = $result->fetchArray()) {
+				$table = array();
+				$result2 = $db->query("PRAGMA table_info(".$row['name'].")");
+				while($row2 = $result2->fetchArray()) {
+					$table[] = $row2[1];
+				}
+				$tables[$row['name']] = $table;
+			}
+		}
+	
+		return $tables;
+	}
+
+	private function queryManifest($query) {
+		$database = Cache::get('database');
+		$cacheFilePath = '../storage/'.pathinfo($database, PATHINFO_BASENAME);
+	
+		$results = array();
+		if ($db = new SQLite3($cacheFilePath)) {
+			$result = $db->query($query);
+			
+			while($row = $result->fetchArray()) {
+				$key = is_numeric($row[0]) ? sprintf('%u', $row[0] & 0xFFFFFFFF) : $row[0];
+				$results[$key] = json_decode($row[1]);
+			}
+		}
+		return $results;
+	}
+
+	public function getDefinition($tableName, $id = false)
+	{
+		if ($id !== false) {
+			$tables = Cache::get('tables');
+			
+			$key = $tables[$tableName][0];
+			$where = ' WHERE ' . (is_numeric($id) ? $key . '=' . $id . ' OR ' . $key . '=' . ($id-4294967296) : $key . '="' . $id . '"');
+
+			$results = $this->queryManifest('SELECT * FROM ' . $tableName . $where);
+
+			return isset($results[$id]) ? $results[$id] : false;
+		} else {
+			return $this->queryManifest('SELECT * FROM ' . $tableName . ' LIMIT 10');
+		}
 	}
 
 	public function searchDestinyPlayer($membershipType, $displayName)
@@ -64,19 +149,42 @@ class BungieRepository
 		$path = "/Destiny2/SearchDestinyPlayer/{$membershipType}/{$displayName}/";
 
 		$response = $this->execCurl($path);
-		echo json_encode($response);exit;
+		return $response;
 	}
 
 	/*
 	 * membershipType: xbox-1, psn-2, blizzard-4
 	 *
 	 */
-	public function getDestiny2Profile($membershipType, $destinyMembershipId) 
+	public function getDestiny2Profile($membershipType, $destinyMembershipId, $components = 100) 
 	{
-		$path = "/Destiny2/{$membershipType}/Profile/{$destinyMembershipId}/";
+		$path = "/Destiny2/{$membershipType}/Profile/{$destinyMembershipId}/?components={$components}";
+
+		$response = $this->execCurl($path);
+		return $response;
+	}
+
+	public function getHistoricalStatsDefinition()
+	{
+		$path = "/Destiny2/Stats/Definition/";
+
+		$response = $this->execCurl($path);
+		return $response;
+	}
+
+	public function getHistoricalStatsForAccount($membershipType, $destinyMembershipId, $groups = "General,Weapons,Medals")
+	{
+		$path = "/Destiny2/{$membershipType}/Account/{$destinyMembershipId}/Stats/?groups={$groups}";
+
+		$response = $this->execCurl($path);
+		return $response;		
+	}
+
+	public function getActivityHistory($membershipType, $destinyMembershipId, $characterId, $mode, $count = 10, $page = 0)
+	{
+		$path = "/Destiny2/{$membershipType}/Account/{$destinyMembershipId}/Character/{$characterId}/Stats/Activities/?mode={$mode}&count={$count}&page={$page}";
 
 		$response = $this->execCurl($path);
 		echo json_encode($response);exit;
-
 	}
 }
